a1430bc57bd6a34bfdeb634ec358cde39b104db6
Bryan P. Arant <brynarnt@protonmail.com>
2019-10-16 16:13:20 -0700

    streaming works!

--- GuyStatelessWebCore/Hubs/GuyHub.cs
+++ GuyStatelessWebCore/Hubs/GuyHub.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace GuyStatelessWebCore.Hubs
@@ -24,6 +25,23 @@ namespace GuyStatelessWebCore.Hubs
         public void HubInfo()
         {
             Clients.Caller.SendAsync("receiveInfo", $"ConnectionId: {this.Context.ConnectionId}");
+        }
+
+        public async IAsyncEnumerable<int> Counter()
+        {
+            int i = 0;
+            while(true)
+            { 
+                // Check the cancellation token regularly so that the server will stop
+                // producing items if the client disconnects.
+                //cancellationToken.ThrowIfCancellationRequested();
+
+                yield return i++;
+
+                // Use the cancellationToken in other APIs that accept cancellation
+                // tokens so the cancellation can flow down to them.
+                await Task.Delay(1000);
+            }
         }
 
         public void Send(string name, string message)
--- GuyStatelessWebCore/wwwroot/js/guystate.js
+++ GuyStatelessWebCore/wwwroot/js/guystate.js
@@ -23,9 +23,18 @@ connection.on("disconnected", function (message) {
 
 connection.start().then(function () {
     console.log("Started Connection to guyhub.");
-    connection.invoke("HubInfo").catch(function (err) {
-        return console.error("Error with connection result: " + err.toString());
-    });
+    connection.stream("Counter")
+        .subscribe({
+            next: (item) => {
+                console.log(item);
+            },
+            complete: () => {
+                console.log("Stream complete.");
+            },
+            error: (err) => {
+                console.error(err);
+            }
+        });
     //document.getElementById("sendButton").disabled = false;
 }).catch(function (err) {
     return console.error(err.toString());
